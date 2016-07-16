e74caeb81dd92edf0f20111f0b04b0c4fd06c6b0
Raul Villareal <rvillarrcal@cnm.edu>
2016-07-16 10:09:15 -0600

    added milestone-1.php

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -1,7 +1,33 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: STEMulus
- * Date: 7/14/2016
- * Time: 12:49 PM
- */
+<!DOCTYPE HTML>
+	< html lang="en">
+		<head>
+			<meta charset="UTF-8">
+			<title> a2r2emd </title>
+		</head>
+		<body>
+			<div class="heading">
+				<h1> a2r2em.com </h1>
+				<p>Developers at your service;</p>
+			</div>
+			<div class="purpose, audience and goal">
+				<h2> Purpose </h2>
+				<p> The purpose of this page is to promote our; professional, personal and human skills, values and beliefs, so that we  attract attention from the customers that share such beliefs and values.</p>
+				<h2> Audience </h2>
+				<p> The ideal audience for our business are:</p>
+				<ul>
+					<li>Local business owners trying to promote their business.</li>
+					<li>Non-profit organizations that can expand their operations exponentially through their own website</li>
+					<li>Entrepreneurs whose ideas require a website to run its business</li>
+					<li>Startups that might contract us to develop a specific rpoject or part of their project</li>
+				</ul>
+				<h2>Goal</h2>
+				<ol>
+					<li>The main objective of this website is to reach as many people as possible. </li>
+					<li>Secondly, we need those viewers to initiate contact by sending us an email, callinng us or filling a contact form.<li>
+					<li>And, if possible, provide them with a general idea of what we offer.</li>
+				</ol>
+			</div>
+
+		</body>
+
+ 
