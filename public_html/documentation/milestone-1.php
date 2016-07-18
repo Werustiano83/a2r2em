cc587e0976d2e09e2d772d61937e3f63d0e92949
Raul Villareal <rvillarrcal@cnm.edu>
2016-07-18 10:11:38 -0600

    added css folder and style file

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -16,22 +16,22 @@
 				<h2> Purpose, audience and goal </h2>
 
 					<h3>Purpose</h3>
-						<p> The purpose of this page is to promote our; professional, personal and human skills, values and beliefs, so that we  attract attention from the customers that share such beliefs and values.</p>
+						<p> The purpose of this page is to promote our professional, personal and human skills, as well as our values and beliefs, so that we  attract attention from customers that share such beliefs and values.</p>
 
 					<h3> Audience </h3>
 						<p> The ideal audience for our business are:</p>
 						<ul>
 							<li>Local business owners trying to promote their business.</li>
-							<li>Non-profit organizations that can expand their operations exponentially through their own website</li>
-							<li>Entrepreneurs whose ideas require a website to run its business</li>
-							<li>Startups that might contract us to develop a specific project or part of their project</li>
+							<li>Non-profit organizations that can expand their operations exponentially through their own website.</li>
+							<li>Entrepreneurs whose ideas require a website to run its business.</li>
+							<li>Startups that might contract us to develop a specific project or part of their existing project.</li>
 						</ul>
 
 					<h3>Goal</h3>
 						<ol>
 							<li>The main objective of this website is to reach as many people as possible. </li>
-							<li>Secondly, we need those viewers to initiate contact by sending us an email, callinng us or filling a contact form.</li>
-							<li>And, if possible, provide them with a general idea of what we offer.</li>
+							<li>Secondly, we need those viewers to initiate contact by sending us an email, calling us or filling a contact form.</li>
+							<li>And provide them with a general idea of what we offer.</li>
 						</ol>
 			</div>
 
@@ -40,10 +40,10 @@
 					<p><strong>Name:</strong> William Goodman (Mr. GoodWill)</p>
 					<p><strong>Age:</strong> Millenial cohort</p>
 					<p><strong>Ocupation:</strong> Mr. GoodWill is the founder of a nonprofit called "Homeless Empowerment", which provides a lot of services to homeless persons. Such services include but are not limited to, food, temporary shelter, temporary work, job skills classes, GED classes and sometimes addiction recovery.</p>
-					<p><strong>Technology:</strong> Mr. GoodWill, is a Windows 8 user. Since he attends a lot of meetings during the day, he has an HP Pavillion Lap Top with a 15.6 screen, and a Surface Pro 3 tablet. He carries both and uses the Surface during meetings to take notes and the Lap Top to perform other aspects of their aspect.</p>
-					<p><strong>Attitudes and Behaviors:</strong> William is very busy looking for money. That is his main priority and the survival of his non-profit is essencial for any other project to take place. He attends a lot of meeting in order to fulfill such purpose and has very little time for all the other items in his calendar. He will do research in Linkedin and will dedicate about a minute looking at web developers portfolio in order to make a choice about who to contact. He's not in a hurry, but the sooner he can make his website happen the better.</p>
-					<p><strong>Frustrations and needs:</strong> William will need to contract a company that understands his project  very well, he knows there's a lot of wordpress "experts" and developers out there that can do a great job, but he wants a real commitment from the developers towards the project. Which means they will understand the values behind it and will have a greater level of commitment than anybody else. Nevertheless, he has set aside a sufficient budget to cover this expenses and is willing to reward properly to the chosen company.</p>
-					<p><strong>Goal:</strong>William will do a lot of reseach through the linkedin website, but he won't make a lot of calls. He has decided to make only 5 calls after he's finished researching based on the portfolio of the developers and whatever information he can find in their website. Such calls will be only 3 minute long and he will only get answers to his specific questions.</p>
+					<p><strong>Technology:</strong> Mr. GoodWill, is a Windows 8 user. Since he attends a lot of meetings during the day, he has an HP Pavillion Lap Top with a 15.6 screen, and a Surface Pro 3 tablet. He carries both and uses the Surface during meetings to take notes and the Lap Top to perform other aspects of their work.</p>
+					<p><strong>Attitudes and Behaviors:</strong> William is very busy looking for money. Fundraising is his main priority, and the survival of his non-profit is essential for any other project to take place. He attends a lot of meeting in order to fulfill such purpose and has very little time for all the other items in his calendar. He will do research in Linkedin and will dedicate about a minute looking at web developers portfolio in order to make a choice about who to contact. He's not in a hurry, but the sooner he can make this website happen the better.</p>
+					<p><strong>Frustrations and needs:</strong> William will need to contract a company that understands his project  very well, he knows there are a lot of wordpress "experts" and developers out there that can do a great job, but he wants a real commitment from the developers towards the project. Which means they will understand the values behind it and will have a greater level of commitment than anybody else. Budget wise, he has set aside a sufficient funds to cover the website expenses and is willing to reward properly to the chosen company.</p>
+					<p><strong>Goal:</strong>William will do a lot of reseach through the linkedin website, but he won't make a lot of calls. He has decided to make only 5 calls after he's finished researching based on the portfolio of the developers and whatever information he can find in their website. Such calls will be only 3 minutes long and he will only get answers to his specific questions.</p>
 			</div>
 
 			<div class="useCase">
