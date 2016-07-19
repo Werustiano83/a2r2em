9bf47d289aafd5f2ae3fa0e236ac519f75331257
Raul Villareal <rvillarrcal@cnm.edu>
2016-07-19 13:02:36 -0600

    created mileston-2.php

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -1,64 +1,91 @@
 <!DOCTYPE HTML>
-	<html lang="en">
+<html lang="en">
 
-		<head>
-			<meta charset="UTF-8">
-			<title> a2r2emd </title>
-		</head>
+	<head>
+		<meta charset="UTF-8">
+		<title> a2r2emd </title>
+	</head>
 
-		<body>
-			<div class="heading">
-				<h1> a2r2em.com </h1>
-				<p>Developers at your service;</p>
+	<body>
+		<div class="heading">
+			<h1> a2r2em.com </h1>
+			<p>Developers at your service;</p>
 
 			<!-- Website's purpose, audience and goal-->
-			</div>
-			<div class="Introduction">
-				<h2> Purpose, audience and goal </h2>
+		</div>
+		<div class="Introduction">
+			<h2> Purpose, audience and goal </h2>
 
-					<h3>Purpose</h3>
-						<p> The purpose of this page is to promote our professional, personal and human skills, as well as our values and beliefs, so that we  attract attention from customers that share such beliefs and values.</p>
+			<h3>Purpose</h3>
+			<p> The purpose of this page is to promote our professional, personal and human skills, as well as our values
+				and beliefs, so that we attract attention from customers that share such beliefs and values.</p>
 
-					<h3> Audience </h3>
-						<p> The ideal audience for our business are:</p>
-							<ul>
-								<li>Local business owners trying to promote their business.</li>
-								<li>Non-profit organizations that can expand their operations exponentially through their own website.</li>
-								<li>Entrepreneurs whose ideas require a website to run its business.</li>
-								<li>Startups that might contract us to develop a specific project or part of their existing project.</li>
-							</ul>
+			<h3> Audience </h3>
+			<p> The ideal audience for our business are:</p>
+			<ul>
+				<li>Local business owners trying to promote their business.</li>
+				<li>Non-profit organizations that can expand their operations exponentially through their own website.</li>
+				<li>Entrepreneurs whose ideas require a website to run its business.</li>
+				<li>Startups that might contract us to develop a specific project or part of their existing project.</li>
+			</ul>
 
-					<h3>Goal</h3>
-						<ol>
-							<li>The main objective of this website is to reach as many people as possible. </li>
-							<li>Secondly, we need those viewers to initiate contact by sending us an email, calling us or filling a contact form.</li>
-							<li>And provide them with a general idea of what we offer.</li>
-						</ol>
-			</div>
+			<h3>Goal</h3>
+			<ol>
+				<li>The main objective of this website is to reach as many people as possible.</li>
+				<li>Secondly, we need those viewers to initiate contact by sending us an email, calling us or filling a
+					contact form.
+				</li>
+				<li>And provide them with a general idea of what we offer.</li>
+			</ol>
+		</div>
 
-			<!--persona, who can be my client?-->
-			<div class="persona">
-				<h2> Persona </h2>
-					<p><strong>Name:</strong> William Goodman (Mr. GoodWill)</p>
-					<p><strong>Age:</strong> Millenial cohort</p>
-					<p><strong>Ocupation:</strong> Mr. GoodWill is the founder of a nonprofit called "Homeless Empowerment", which provides a lot of services to homeless persons. Such services include but are not limited to, food, temporary shelter, temporary work, job skills classes, GED classes and sometimes addiction recovery.</p>
-					<p><strong>Technology:</strong> Mr. GoodWill, is a Windows 8 user. Since he attends a lot of meetings during the day, he has an HP Pavillion Lap Top with a 15.6 screen, and a Surface Pro 3 tablet. He carries both and uses the Surface during meetings to take notes and the Lap Top to perform other aspects of their work.</p>
-					<p><strong>Attitudes and Behaviors:</strong> William is very busy looking for money. Fundraising is his main priority, and the survival of his non-profit is essential for any other project to take place. He attends a lot of meeting in order to fulfill such purpose and has very little time for all the other items in his calendar. He will do research in Linkedin and will dedicate about a minute looking at web developers portfolio in order to make a choice about who to contact. He's not in a hurry, but the sooner he can make this website happen the better.</p>
-					<p><strong>Frustrations and needs:</strong> William will need to contract a company that understands his project  very well, he knows there are a lot of wordpress "experts" and developers out there that can do a great job, but he wants a real commitment from the developers towards the project. Which means they will understand the values behind it and will have a greater level of commitment than anybody else. Budget wise, he has set aside a sufficient funds to cover the website expenses and is willing to reward properly to the chosen company.</p>
-					<p><strong>Goal:</strong>William will do a lot of reseach through the linkedin website, but he won't make a lot of calls. He has decided to make only 5 calls after he's finished researching based on the portfolio of the developers and whatever information he can find in their website. Such calls will be only 3 minutes long and he will only get answers to his specific questions.</p>
-			</div>
+		<!--persona, who can be my client?-->
+		<div class="persona">
+			<h2> Persona </h2>
+			<p><strong>Name:</strong> William Goodman (Mr. GoodWill)</p>
+			<p><strong>Age:</strong> Millenial cohort</p>
+			<p><strong>Ocupation:</strong> Mr. GoodWill is the founder of a nonprofit called "Homeless Empowerment", which
+				provides a lot of services to homeless persons. Such services include but are not limited to, food,
+				temporary shelter, temporary work, job skills classes, GED classes and sometimes addiction recovery.</p>
+			<p><strong>Technology:</strong> Mr. GoodWill, is a Windows 8 user. Since he attends a lot of meetings during
+				the day, he has an HP Pavillion Lap Top with a 15.6 screen, and a Surface Pro 3 tablet. He carries both and
+				uses the Surface during meetings to take notes and the Lap Top to perform other aspects of their work.</p>
+			<p><strong>Attitudes and Behaviors:</strong> William is very busy looking for money. Fundraising is his main
+				priority, and the survival of his non-profit is essential for any other project to take place. He attends a
+				lot of meeting in order to fulfill such purpose and has very little time for all the other items in his
+				calendar. He will do research in Linkedin and will dedicate about a minute looking at web developers
+				portfolio in order to make a choice about who to contact. He's not in a hurry, but the sooner he can make
+				this website happen the better.</p>
+			<p><strong>Frustrations and needs:</strong> William will need to contract a company that understands his
+				project very well, he knows there are a lot of wordpress "experts" and developers out there that can do a
+				great job, but he wants a real commitment from the developers towards the project. Which means they will
+				understand the values behind it and will have a greater level of commitment than anybody else. Budget wise,
+				he has set aside a sufficient funds to cover the website expenses and is willing to reward properly to the
+				chosen company.</p>
+			<p><strong>Goal:</strong>William will do a lot of reseach through the linkedin website, but he won't make a lot
+				of calls. He has decided to make only 5 calls after he's finished researching based on the portfolio of the
+				developers and whatever information he can find in their website. Such calls will be only 3 minutes long and
+				he will only get answers to his specific questions.</p>
+		</div>
 
-			<!-- useCase, when my services will be required-->
-			<div class="useCase">
-				<h2> Use Case </h2>
-					<p>In his years of experience he learned about a lot of websites and mobile apps that could be helpful to a lot of the homeless people in order for them to obtain, education, job skills, food and shelter among other things. Because of this, he wants to create a program where homeless people can access these services through a website that puts all of these websites and mobile apps together. Since we're talking about homeless people, William needs to figure out a way to get them smartphones with wifi capability. Another option it would be to get them access to library cards and they will be using the library desktops. For our purposes we need to take a universal approach with regards to the technology that will be used to access our product. Such technologyes include but are not limited to:</p>
-					<ul>
-						<li>Library desktops ranging from Windows Vista to Windows 10</li>
-						<li>Library Macintosh with Mac OS X versions 10.6 (Snow Leopard) to 10.11 (El Capitan)</li>
-						<li>Smartphones with Android Operative System versions 2.3 to 6.0</li>
-						<li>Iphones with iOS Operative System versions 2.0 to 8.0</li>
-					</ul>
-			</div>
-		</body>
+		<!-- useCase, when my services will be required-->
+		<div class="useCase">
+			<h2> Use Case </h2>
+			<p>In his years of experience he learned about a lot of websites and mobile apps that could be helpful to a lot
+				of the homeless people in order for them to obtain, education, job skills, food and shelter among other
+				things. Because of this, he wants to create a program where homeless people can access these services
+				through a website that puts all of these websites and mobile apps together. Since we're talking about
+				homeless people, William needs to figure out a way to get them smartphones with wifi capability. Another
+				option it would be to get them access to library cards and they will be using the library desktops. For our
+				purposes we need to take a universal approach with regards to the technology that will be used to access our
+				product. Such technologyes include but are not limited to:</p>
+			<ul>
+				<li>Library desktops ranging from Windows Vista to Windows 10</li>
+				<li>Library Macintosh with Mac OS X versions 10.6 (Snow Leopard) to 10.11 (El Capitan)</li>
+				<li>Smartphones with Android Operative System versions 2.3 to 6.0</li>
+				<li>Iphones with iOS Operative System versions 2.0 to 8.0</li>
+			</ul>
+		</div>
+	</body>
 
- 	</html>
+</html>
