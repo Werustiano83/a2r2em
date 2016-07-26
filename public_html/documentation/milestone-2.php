c9fad04c5e6fe0eab9a2ddef4bec2257029947f3
Raul Villareal <rvillarrcal@cnm.edu>
2016-07-25 20:45:39 -0600

    Mileston 2 first draft ready

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -2,27 +2,38 @@
 <html>
 	<head>
 		<meta charset="UTF-8">
-		<title>A2R2EM milestone 2</title>
+		<title>my-pwp milestone 2</title>
 	</head>
 
-		<body>
-			<div class="heading">
-				<h1> a2r2em.com </h1>
-				<p>Developers at everybody's service;</p>
+	<body>
+		<div class="heading">
+			<h1> a2r2em.com </h1>
+			<p>Developers at everybody's service;</p>
+		</div>
 
-				<!-- Website's sitemap, content strategy amd wireframe-->
-			</div>
-			<div class="Sitemap">
-				<h2> Sitemap </h2>
+		<div class="Sitemap">
+			<h2> Sitemap </h2>
+			<img src="images\A2R2EMSiteMapping.png" width="600">
 
+			<h2> Content Strategy </h2>
+			<h3> Home Page </h3>
+			<p> This section will described briefly the kind of work I do as well as invite clients to reach us through the
+				website or social media. It will also show some of the work I admire and would like to be part of in order
+				to demostrate the kind of values that I share with other entrepreneurs. </p>
+			<h3> About </h3>
+			<p> This section will provide the client with a little history about the company.</p>
+			<h3> Portfolio </h3>
+			<p>In this page clients will see the work that we have done in the past in order for them to evaluate our
+				skillset and abilities.</p>
+			<h3> Blog </h3>
+			<p>This will be the part where we'll share news about the company or other topics related to the technological
+				field, along with our perpectives about such.</p>
+			<h3> Contact</h3>
+			<p> In this part client will be able to cantact us through a friendly contact form that we will provide for
+				them.</p>
 
-				<h2> Content Strategy </h2>
-				<h3> Home Page </h3>
-				<h3> About </h3>
-				<h3> Portfolio </h3>
-				<h3> Blog </h3>
-				<h3> Contact</h3>
-			</div>
-
-		</body>
+			<h2>Wireframe</h2>
+			<img src="images\wireframea2r2emfinal.png" width="600">
+		</div>
+	</body>
 </html>
