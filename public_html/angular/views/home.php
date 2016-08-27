9a64e42fb136ae546b4ba86a5fed37858ebbe2e3
Raul Villareal <rvillarrcal@cnm.edu>
2016-08-27 14:13:08 -0600

    added content to home.php

---
+++ public_html/angular/views/home.php
@@ -0,0 +1,13 @@
+<div class=""row">
+	<div class="col-xs-12">
+		<div class="jumbotron text-left">
+			<h1><i class="wev-a-tion"></i>wev-a-tion</h1>
+			<p class="slogan">Developing web solutions. Expanding web education.</p>
+		</div>
+	</div>
+</div>
+
+
+
+
+
