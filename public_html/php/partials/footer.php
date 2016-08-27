704e3daa64a8a4f256de5dac42d4b95514d09b82
Raul Villareal <rvillarrcal@cnm.edu>
2016-08-27 17:33:39 -0600

    created /partials/header.php

---
+++ public_html/php/partials/footer.php
@@ -0,0 +1,10 @@
+<!--website footer -->
+<footer>
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-xs-12">
+				&copy;&nbsp;2016 web-a-tion
+			</div>
+		</div>
+	</div>
+</footer>
