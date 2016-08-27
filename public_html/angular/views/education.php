2d4f54d7d0c97998c223ac07b2cd200011e939d9
Raul Villareal <rvillarrcal@cnm.edu>
2016-08-27 15:09:53 -0600

    added content in education.php

---
+++ public_html/angular/views/education.php
@@ -0,0 +1,28 @@
+<h1>Parents College</h1>
+</div>
+</div>
+
+<div class="row">
+	<div class="col-sm-4">
+		<img src="../../images/background.jpg" alt="background" class="img-thumbnail pull-left">
+	</div>
+	<div class="col-sm-6">
+		<p>The concept is as old as human race... parents taking care of their children. The only thing we added to it is the use of technology. We use three main resources for families to learn anything ty want.</p>
+		<p>We also believe in expandig web education. Working with families that want to learn web development is part of the work we do. Parents and their kids, no matter the edge are getting trained in computer programming through our program Parents College. To learn more, click on our education button in the top menu.</p>
+	</div>
+	<div class="container-fluid text-center">
+		<div class="row">
+			<div class="col-sm-3">
+				<img class="img-responsive img-circle" src="../../images/background.jpg"/>
+				<p>Khan Academy currently is the largest University on the planet. Through their very friendly video tutorials and amazing features, learning has become a child's play. Nevertheless, our adult participants enjoy using it a lot too. When adulst and kids are using Khan Academy together the knowledge is in the air...</p>
+			</div>
+			<div class="col-sm-3">
+				<img class="img-responsive img-circle" src="../../images/background.jpg"/>
+				<p>Code.org is an amazing site for learning computer programming. This website is very fun to use and its game driven methodology makes the line between learning and having fun very blurry.</p>
+			</div>
+			<div class="col-sm-3">
+				<img class="img-responsive img-circle" src="../../images/background.jpg"/>
+				<p>In this globalized world where is more necessary every time to learn new languages, a free solution for language learning is duolingo.com. Choose your language and let them know your primary language and in a very intuitive way you will be learning the four main aspects of the language you choose. They have a wide variaty of them for you to take advantage of this website and app.</p>
+			</div>
+
+		</div>
