75b9ed904ea2f6a3b1b431aab4bd7e20511a4460
Raul Villareal <rvillarrcal@cnm.edu>
2016-08-27 14:30:39 -0600

    added content for about.php

---
+++ public_html/angular/views/about.php
@@ -0,0 +1,15 @@
+<div class="row">
+	<div class="col-xs-12">
+		<h2>This is what we like doing!</h2>
+	</div>
+</div>
+
+<div class="row">
+	<div class="col-sm-6">
+		<img src="../../images/background.jpg" alt="background" class="img-thumbnail pull-left">
+	</div>
+	<div class="col-sm-9">
+		<p>Whether you're a local business owner, a blogger, non-profit or a big corporation; having a customized website according to your needs can help you expand your brand, purpose or business. We're convinced that a there's no idea that is too smal to create impact in the world nor idea too big to be possible. We want to hear from you and talk to you about the advantages of having an updated, customized website as well as the difference it can make fr your business.</p>
+		<p>We also believe in expandig web education. Working with families that want to learn web development is part of the work we do. Parents and their kids, no matter the edge are getting trained in computer programming through our program Parents College. To learn more, click on our education button in the top menu.</p>
+	</div>
+</div>
