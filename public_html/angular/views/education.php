bf75c74503f58925d3319ab210654f150aa76f3d
Raul Villareal <rvillarrcal@cnm.edu>
2016-08-27 19:04:46 -0600

    little modifications

--- public_html/angular/views/education.php
+++ public_html/angular/views/education.php
@@ -4,7 +4,7 @@
 
 <div class="row">
 	<div class="col-sm-6">
-		<img src="../../images/background.jpg" alt="background" class="img-thumbnail pull-left">
+		<img src="../../images/thunder.jpg" width="800" alt="background" class="img-thumbnail pull-left">
 	</div>
 	<div class="col-sm-6">
 		<p>The concept is as old as the human race... parents taking care of their children. The only thing we added to it is the use of technology. We use three main resources for families to learn anything ty want.</p>
@@ -13,15 +13,15 @@
 	<div class="container-fluid text-center">
 		<div class="row">
 			<div class="col-sm-4" align="left">
-				<img class="img-responsive img-circle" src="../../images/background.jpg"/>
+				<img class="img-responsive" src="../../images/khanlogo.png"/>
 				<p>Khan Academy currently is the largest University on the planet. Through their very friendly video tutorials and amazing features this website has helped millions of people and learning has become a child's play. Additionally, our adult participants enjoy using it a lot too. When adulst and kids are using Khan Academy together the knowledge is in the air...</p>
 			</div>
 			<div class="col-sm-4">
-				<img class="img-responsive img-circle" src="../../images/background.jpg"/>
+				<img class="img-responsive" src="../../images/codelogo.png"/>
 				<p>Code.org is an amazing site for learning computer programming. This website is very fun to use and its game driven methodology makes the line between learning and having fun very blurry.</p>
 			</div>
 			<div class="col-sm-4">
-				<img class="img-responsive img-circle" src="../../images/background.jpg"/>
+				<img class="img-responsive" src="../../images/duolingologo.png"/>
 				<p>In this globalized world where is more necessary every time to learn new languages, a free solution for language learning is duolingo.com. Choose your language and let them know your primary language and in a very intuitive way you will be learning the four main aspects of the language you choose. They have a wide variaty of them for you to take advantage of this website and app.</p>
 			</div>
 
