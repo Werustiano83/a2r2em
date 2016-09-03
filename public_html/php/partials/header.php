3771342efd66df238fe931f0fe7be2017e10e04c
Raul Villareal <rvillarrcal@cnm.edu>
2016-09-03 11:48:08 -0600

    nav bar is fixed now

--- public_html/php/partials/header.php
+++ public_html/php/partials/header.php
@@ -13,7 +13,7 @@
 					<span class="icon-bar"></span>
 					<span class="icon-bar"></span>
 				</button>
-				<a class="navbar-brand" href="index">wev-a-tion</a>
+				<a class="navbar-brand" href="index"><img src="images/weva" width="65"></a>
 			</div>
 
 			<!-- Collect the nav links, forms, and other content for toggling -->
